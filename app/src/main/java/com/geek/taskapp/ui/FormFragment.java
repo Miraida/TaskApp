5fcfdf1bead25519af3b0ef034caf5ee3ab93de2
Miraida <miraidajaanbaeva@gmail.com>
2021-04-09 14:05:00 +0600

    Initial commit

---
+++ app/src/main/java/com/geek/taskapp/ui/FormFragment.java
@@ -0,0 +1,37 @@
+package com.geek.taskapp.ui;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.geek.taskapp.R;
+
+public class FormFragment extends Fragment {
+    EditText editText;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_form, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        editText = view.findViewById(R.id.editText);
+        view.findViewById(R.id.add_button).setOnClickListener(v->{
+            save();
+        });
+    }
+
+    private void save() {
+        String text = editText.getText().toString();
+    }
+}
---
+++ app/src/main/java/com/geek/taskapp/ui/dashboard/DashboardFragment.java
@@ -0,0 +1,35 @@
+package com.geek.taskapp.ui.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+
+import com.geek.taskapp.R;
+
+public class DashboardFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_dashboard, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        openProfile();
+    }
+    private void openProfile() {
+        NavController navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment);
+        navController.navigate(R.id.profileFragment);
+    }
+}
